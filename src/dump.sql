fea1af4d4c210dc6b7ac1987648cf4395dca2210
kvasenko <kvasenko@ukr.net>
2016-10-25 16:16:18 +0300

    Added dump.sql

---
+++ src/dump.sql
@@ -0,0 +1,24 @@
+# Media for all modules
+CREATE TABLE media
+(
+  id BIGINT(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  userId BIGINT(20) unsigned NOT NULL,
+  module VARCHAR(24) DEFAULT 'users' NOT NULL,
+  title LONGTEXT,
+  type VARCHAR(24),
+  file VARCHAR(255),
+  thumb VARCHAR(255),
+  size INT(11) unsigned NOT NULL,
+  created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated TIMESTAMP NOT NULL,
+  CONSTRAINT media_users_id_fk FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+LOCK TABLES `acl_privileges` WRITE;
+
+INSERT INTO `acl_privileges` (`roleId`, `module`, `privilege`)
+VALUES
+  (2,'media','Management'),
+  (2,'media','Upload'),
+  (3,'media','Upload')
+;
